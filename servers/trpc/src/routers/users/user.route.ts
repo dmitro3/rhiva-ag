71640aaae639084f0d91e6ed864ea9cc08a864b5
oasisMystre <oasis.mystre@gmail.com>
2025-11-06 19:23:14 +0100

    bug-fix: import error

--- servers/trpc/src/routers/users/user.route.ts
+++ servers/trpc/src/routers/users/user.route.ts
@@ -2,9 +2,9 @@ import { eq } from "drizzle-orm";
 import { TRPCError } from "@trpc/server";
 import { users, userUpdateSchema } from "@rhiva-ag/datasource";
 
+import { getUserById } from "./user.controller";
 import { privateProcedure, router } from "../../trpc";
 import { extendedUserSelectSchema } from "./user.schema";
-import { getUserById } from "./user.controller";
 
 export const userRoute = router({
   me: privateProcedure

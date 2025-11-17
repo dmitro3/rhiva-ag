bd644697322ebbba5d79438c811b4a111eceaa09
oasisMystre <oasis.mystre@gmail.com>
2025-11-17 19:48:43 +0100

    feat: bug-fixes

--- apps/www/open-next.config.ts
+++ apps/www/open-next.config.ts
@@ -1,6 +1,10 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
+import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
 import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 
 export default defineCloudflareConfig({
+  queue: doQueue,
+  tagCache: d1NextTagCache,
   incrementalCache: kvIncrementalCache,
 });

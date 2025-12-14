ed10bcfc84346e75e00412a5b64ca9c9ff13c30a
oasisMystre <oasis.mystre@gmail.com>
2025-12-14 15:07:23 +0100

    feat: bug fix

--- build.dev.sh
+++ build.dev.sh
@@ -37,7 +37,7 @@ sudo docker push $REGISTRY_URL/rhiva-ag/servers:$TAG
 kubectl create secret generic regcred \
 --from-file=.dockerconfigjson=$HOME/.docker/config.json \
 --namespace $NAMESPACE \
---type=kubernetes.io/dockerconfigjson\
+--type=kubernetes.io/dockerconfigjson \
 --dry-run=client -o yaml | kubectl apply -f - 
 
 . ./scripts/k8s-codegen.sh 
